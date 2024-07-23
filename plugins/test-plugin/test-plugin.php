da3e6a1335f22ef7de5de127480d3b270b389358
Paul Gilzow <paul.gilzow@platform.sh>
2024-07-23 12:29:26 -0500

    adds blank plugin so we can use it later to break an e2e test

---
+++ plugins/test-plugin/test-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     test-plugin
+ * Plugin URI:      https://gilzow.com/
+ * Description:     Purposely break an E2E test
+ * Author:          Paul Gilzow
+ * Author URI:      https://gilzow.com/
+ * Text Domain:     test-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         E2E-test
+ */
